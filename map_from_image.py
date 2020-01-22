e6898de7258444c4bc3bb7f8a670dd2f6915e93b
Vring <44764023+Vring@users.noreply.github.com>
2020-01-22 19:08:23 +1000

    Makes txt map file from black and white image

---
+++ map_from_image.py
@@ -0,0 +1,23 @@
+from PIL import Image
+try:
+    im = Image.open("map.jpg")
+except:
+    im = Image.open('map.png')
+f = open('map.txt', mode='w')
+pixels = im.load()
+x, y = im.size
+print(x, y)
+current_line = ''
+for i in range(y):
+    for j in range(x):
+        r, g, b = pixels[j, i]
+        if not r and not g and not b:
+            current_line += '1'
+        else:
+            current_line += '0'
+    f.write(current_line)
+    f.write('\n')
+    current_line = ''
+f.close()
+
+
