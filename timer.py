2531533fdcaf2b80e7bb257b63f99966353c67a9
Vring <44764023+Vring@users.noreply.github.com>
2020-01-31 22:03:22 +1000

    Заготовка таймера хода

---
+++ timer.py
@@ -0,0 +1,7 @@
+import pygame
+start_ticks=pygame.time.get_ticks() #starter tick
+while True: # mainloop
+    seconds=(pygame.time.get_ticks()-start_ticks)/1000 #calculate how many seconds
+    if seconds>10: # if more than 10 seconds close the game
+        break
+    print (seconds) #print how many seconds
