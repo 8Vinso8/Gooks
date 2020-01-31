3669ff855695dffbbf810077a0217b318989952f
Vring <44764023+Vring@users.noreply.github.com>
2020-01-31 22:15:09 +1000

    Add files via upload

--- team.py
+++ team.py
@@ -1,8 +1,8 @@
 class Team:
-    def __init__(self, team_name, team_color, positions):
+    def __init__(self, team_name, team_color, positions, names):
         self.gooks = list()
-        for position in positions:
-            self.gooks.append(Gook(position, team_color))
+        for position, name in positions, names:
+            self.gooks.append(Gook(position, team_color, name))
         self.team_name = team_name
 
     def get_gook(self, n):
